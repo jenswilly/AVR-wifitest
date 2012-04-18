3551d309fa5fbcbc67f4f38c299657f0a7b3fbf0
jenswilly <jens@greenerpastures.dk>
2012-04-18 22:12:39 +0200

    Complete init sequence; debug LED commands implemented
    - added udpHandler
    - added handling of UDP receive with <esc>u…<esc>E
    - added response code INVALID INPUT
    - implemented remote commands LEDON, LEDOFF and LED<num>
    - implemented complete startup sequence for ad hoc network and UDP server

--- wifitest/main.c
+++ wifitest/main.c
@@ -7,6 +7,16 @@
  *
  */
 
+/*
+ * AT					// Are we live?
+ * AT+WM=1				// Mode=ad hoc
+ * AT+DHCPSRVR=1		// Enable DHCP server
+ * AT+WAUTH=2			// WEP security
+ * AT+WWEP1=616c69656e	// passphrase = 'alien'
+ * AT+WA=GS1011M		// Create ad hoc network named 'GS1011M'
+ * AT+NSUDP=49000		// Listen on UDP port 49000
+ */
+
 /* Precompiler stuff for calculating USART baud rate value 
  * Otherwise, use this page: http://www.wormfood.net/avrbaudcalc.php?postbitrate=9600&postclock=12&bit_rate_table=on
  */
@@ -29,8 +39,11 @@
 // Global vars
 #define RX_BUF_SIZE	1024
 char usartBuffer[RX_BUF_SIZE];				// USART receive buffer
-volatile unsigned char usartPtr;			// USART buffer pointer
+volatile int usartPtr;						// USART buffer pointer
+volatile int usartLinePtr;					// USART buffer pointer to beginning of line
 volatile unsigned char responseCode;		// Last response code
+void (*lineHandler)(const char*) = 0;		// Line handler function pointer (returns nothing; takes a pointer to immutable chars)
+void (*udpHandler)(const char*) = 0;		// UDP server data handler function pointer (returns nothing; takes a pointer to immutable chars)
 
 // Serial stream
 static int uart_putchar( char c, FILE *stream );
@@ -41,20 +54,47 @@ FILE usart = FDEV_SETUP_STREAM( uart_putchar, NULL, _FDEV_SETUP_WRITE );
 ISR( USART_RX_vect ) 
 { 
 	unsigned char byte;
+	int i;
 	
 	// Prevent buffer overflow
 	if( usartPtr >= RX_BUF_SIZE-1 )
-		usartPtr = 0;
+		resetBuffer();
 	
 	// Grab the data and but it into the buffer and increase pointer
 	byte = UDR0;
 	usartBuffer[ usartPtr++ ] = byte;
 
-	/*
-	// Ignore CR and LF
-	if( byte != 0x0A && byte != 0x0D )
-		usartBuffer[ usartPtr++ ] = byte;
-	*/
+	// If we have received \n, we have en entire line
+//	if( byte == '\n' )
+//	{
+//		// If we have a line handler, pass the line to be handled
+//		if( lineHandler != 0 )
+//			lineHandler( usartBuffer+usartLinePtr );	// Pass a pointer to the beginning of the current line
+//		
+//		// New line begins here
+//		usartLinePtr = usartPtr;
+//	}
+	
+	if( byte == 'E' && usartBuffer[ usartPtr-2 ] == 27 )
+	{
+		// <esc>E received: end of UDP server reception. Do we have a UDP server handler?
+		if( udpHandler != 0 )
+		{
+			// Yes: scan back until we find the begin marker <esc>u
+			for( i=usartPtr; i >= 0; i-- )
+				if( usartBuffer[ i ] == 27 && usartBuffer[ i+1 ] == 'u' )
+					break;
+			
+			// Make sure the string is 0-terminated
+			usartBuffer[ usartPtr ] = 0;
+			
+			// Pass it to the handler
+			udpHandler( usartBuffer+i );
+			
+			// Reset buffer
+			usartPtr = 0;
+		}
+	}
 	
 	// Check for known status codes. Some commands might return a lot of text so we look at the last X chars and match against the known
 	// response codes.
@@ -76,6 +116,14 @@ ISR( USART_RX_vect )
 		// Reset buffer
 		usartPtr = 0;
 	}
+	// 
+	else if( usartPtr >= 24 && strncmp_P( usartBuffer + usartPtr-24, PSTR("\r\nERROR: INVALID INPUT\r\n"), 24 ) == 0 )
+	{
+		responseCode = S2W_EINVAL;
+		
+		// Reset buffer
+		usartPtr = 0;
+	}
 }
 
 // Stream handler so we can use fprintf( &usart, ... ) to print serial data
@@ -88,6 +136,12 @@ static int uart_putchar(char c, FILE *stream)
 	// Send byte
 	UDR0 = c;
 	return 0;
+}
+
+void resetBuffer()
+{
+	usartPtr = 0;							// Point af beginning of buffer
+	usartLinePtr = 0;						// Ditto for line pointer
 }
 
 int send_serial_command_P( const prog_char *fmt, ... )
@@ -104,6 +158,62 @@ int send_serial_command_P( const prog_char *fmt, ... )
 	
 	// Return response code
 	return responseCode;
+}
+
+void receiveUdp( const char* buffer )
+{
+	int indx = 0;
+	int indx2 = 0;
+	char host[16];
+	unsigned int port=0;
+	char msg[32];
+	
+	// We get the entire <esc>u<CID><source IP address><space><port><htab><data><esc>E string and we are only interested in source IP and data.
+
+	// Scan past leading <esc>u
+	while( buffer[indx] < '0' || buffer[indx] > '9' )
+		indx++;
+	
+	// Skip CID
+	indx++;
+	
+	// Copy source IP
+	while( buffer[indx] != ' ' )
+		host[indx2++] = buffer[indx++];
+	host[indx2] = 0;	// Terminate
+	
+	// Scan up to data
+	while( buffer[indx] != '\t' )
+		indx++;
+	
+	// Skip tab
+	indx++;
+	
+	// Copy message
+	indx2 = 0;
+	while( buffer[indx] != 27 && buffer[indx] != 0 )
+		msg[indx2++] = buffer[indx++];
+	msg[indx2] = 0;	// Terminate
+	
+	if( strncmp_P( msg, PSTR("LEDON"), 5 ) == 0 )
+		// LED ON
+		DEBUG_PORT |= (1<< DEBUG_BIT);
+	
+	if( strncmp_P( msg, PSTR("LEDOFF"), 6 ) == 0 )
+		// LED OFF
+		DEBUG_PORT &= ~(1<< DEBUG_BIT);
+	
+	if( strncmp_P( msg, PSTR("LED"), 3 ) == 0 && msg[3] >= '0' && msg[3] <= '9' )
+	{
+		indx2 = msg[3] - '0';
+		for( indx = 0; indx < indx2; indx++ )
+		{
+			DEBUG_PORT |= (1<< DEBUG_BIT);
+			_delay_ms( 100 );
+			DEBUG_PORT &= ~(1<< DEBUG_BIT);
+			_delay_ms( 100 );
+		}
+	}
 }
 
 /* This method flashes the debugging LED the specified number of times.
@@ -127,6 +237,9 @@ void setup_ports()
 	// Debug LED -> output
 	DEBUG_DDR |= (1<< DEBUG_BIT);
 	DEBUG_PORT &= ~(1<< DEBUG_BIT);	// LED off
+	
+	DDRB |= (1<< PB4);	/// TEMP: use PB4 as GND for debugging LED
+	PORTB &= ~(1<< PB4);
 }
 
 void setup_interrupts()
@@ -148,6 +261,8 @@ void setup_serial()
 
 int main(void)
 {
+	int step;
+	
 	// Initialize
 	setup_ports();
 	setup_serial();
@@ -156,12 +271,61 @@ int main(void)
 	
 	// Wait a bit to let the WiFi module power up
 	_delay_ms( 1000 );
-	
+	DEBUG_PORT |= (1<< DEBUG_BIT);
+	_delay_ms( 1000 );
+	DEBUG_PORT &= ~(1<< DEBUG_BIT);
+	_delay_ms( 1000 );
+
+
+	/*
+	 * AT					// Are we live?
+	 * AT+WM=1				// Mode=ad hoc
+	 * AT+DHCPSRVR=1		// Enable DHCP server
+	 * AT+WAUTH=2			// WEP security
+	 * AT+WWEP1=616c69656e	// passphrase = 'alien'
+	 * AT+WA=GS1011M		// Create ad hoc network named 'GS1011M'
+	 * AT+MCSTSET=1			// enable multicast reception
+	 * AT+NSUDP=49000		// Listen on UDP port 49000
+	 */
+
 	// Let's see if it works at all
-	if( send_serial_command_P( PSTR("AT\r") ) == S2W_SUCCESS )
-		debug_error( 1 );
-	else
-		debug_error( 2 );
+	step = 1;
+	if( !send_serial_command_P( PSTR("AT\r") ) == S2W_SUCCESS )
+		goto fail;
+
+	step = 2;
+	if( !send_serial_command_P( PSTR("AT+WM=1\r") ) == S2W_SUCCESS )
+		goto fail;
+	
+	step = 3;
+	if( !send_serial_command_P( PSTR("AT+DHCPSRVR=1\r") ) == S2W_SUCCESS )
+		goto fail;
+	
+	step = 4;
+	if( !send_serial_command_P( PSTR("AT+WAUTH=2\r") ) == S2W_SUCCESS )
+		goto fail;
+	
+	step = 5;
+	if( !send_serial_command_P( PSTR("AT+WWEP1=616c69656e\r") ) == S2W_SUCCESS )
+		goto fail;
+	
+	step = 6;
+	if( !send_serial_command_P( PSTR("AT+WA=GS1011M\r") ) == S2W_SUCCESS )
+		goto fail;
+	
+	step = 7;
+	if( !send_serial_command_P( PSTR("AT+MCSTSET=1\r") ) == S2W_SUCCESS )
+		goto fail;
+
+	step = 8;
+	if( !send_serial_command_P( PSTR("AT+NSUDP=49000\r") ) == S2W_SUCCESS )
+		goto fail;
+	
+	// Set UDP receive handler
+	udpHandler = &receiveUdp;
+	
+fail:
+	debug_error( step );
 	
 	// Main loop
 	while(1)
--- wifitest/main.h
+++ wifitest/main.h
@@ -13,8 +13,11 @@
 #define S2W_PENDING 0	// pseudo-code – means "valid response code has not yet been received". Keep checking until status code != S2W_PENDING.
 #define S2W_SUCCESS 1
 #define S2W_FAILURE 2
+#define S2W_EINVAL  3
 
 // Prototypes
 int send_serial_command_P( const prog_char *fmt, ... ) __attribute__((format(printf, 1, 2)));	// Let compiler check printf style format
+void resetBuffer();
+void receiveUdp( const char* buffer );
 
 #endif
